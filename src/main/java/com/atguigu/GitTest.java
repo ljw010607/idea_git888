c4cacb891c5bf999040671890fa63b9856602ba8
李俊威 <1779149042@qq.com>
2022-06-02 09:27:44 +0800

    第二次更新

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("添加");
         System.out.println("第一个更新");
+        System.out.println("第二个更新");
     }
 }
